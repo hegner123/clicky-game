e1db324ac49900bbb26fba05577dfe905d970b88
Michael Hegner <hegner123@gmail.com>
2019-12-21 22:22:09 -0500

    Updates

---
+++ precache-manifest.29ea91c9becb2fcc3bb801f0ac665550.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9a2e6945324865c7343d0c0bb23e321",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "9758f19a144588c35bab",
+    "url": "/clicky-game/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5c3f4d58afe38700c6cb",
+    "url": "/clicky-game/static/js/2.7463f86d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.7463f86d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9758f19a144588c35bab",
+    "url": "/clicky-game/static/js/main.84892d3c.chunk.js"
+  },
+  {
+    "revision": "76da5bcc0e2cfb61c97a",
+    "url": "/clicky-game/static/js/runtime-main.39dcd64a.js"
+  }
+]);
